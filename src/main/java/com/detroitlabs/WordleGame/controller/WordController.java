dcb1fcadc36b5312d9bc61077a30baf129e0e676
Cameron <cameronhanlin@Camerons-MacBook-Pro.local>
2022-03-30 13:09:34 -0600

    Basic webpages

--- src/main/java/com/detroitlabs/WordleGame/controller/WordController.java
+++ src/main/java/com/detroitlabs/WordleGame/controller/WordController.java
@@ -5,18 +5,28 @@ import com.detroitlabs.WordleGame.model.WordBank;
 import com.detroitlabs.WordleGame.service.WordGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WordController {
-//    WordGeneratorService wordGeneratorService = new WordGeneratorService();
+
     @Autowired
-    LetterRepository letterRepository = new LetterRepository();
+    LetterRepository letterRepository;
 
     @RequestMapping("/")
-    public String displayHome() {
+    public String displayHome(ModelMap modelMap) {
+        modelMap.put("CharBank", letterRepository.getCharacterBanks());
+        modelMap.put("wordLength",letterRepository.getCharacterBanks().size());
         return "home";
 
+    }
+
+    @RequestMapping("/page2")
+    public String displayPage2(@RequestParam String keyword, ModelMap modelMap){
+        modelMap.put("userGuess", keyword);
+        return "page2";
     }
 }
--- src/main/java/com/detroitlabs/WordleGame/data/LetterRepository.java
+++ src/main/java/com/detroitlabs/WordleGame/data/LetterRepository.java
@@ -2,24 +2,29 @@ package com.detroitlabs.WordleGame.data;
 
 import com.detroitlabs.WordleGame.model.CharacterBank;
 import com.detroitlabs.WordleGame.service.WordGeneratorService;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class LetterRepository { //generate word
     //break word into part of characters
     //check character with color response
     WordGeneratorService wordGeneratorService = new WordGeneratorService();
-    List<CharacterBank> characterBanks;
+    ArrayList<CharacterBank> characterBanks = new ArrayList<>();
 
     public LetterRepository() {
         String theWord = wordGeneratorService.getWord()[0];
+        System.out.println(theWord);
         while(theWord.length() > 0) {
             String x = theWord.substring(0,1);
+            theWord = theWord.substring(1);
             characterBanks.add(new CharacterBank(x));
         }
     }
 
-
-
-
+    public List<CharacterBank> getCharacterBanks() {
+        return characterBanks;
+    }
 }
